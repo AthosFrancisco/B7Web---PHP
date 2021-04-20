c00dacddaba300fc3faaf89fd444cf0d3d8ad340
Athos Francisco Pereira do Carmo <athos.carmo@mcmmontagens.com.br>
2021-04-20 12:00:31 -0300

    Exercícios de Fixação de loops. primeiro criar 10 linhas com 10 traços cada. segundo criar 20 linhas com um traço mais a quantidade de traços da linha anterior, começando com 1 traço

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php
 
-$nome = 'Athos';
+for($i = 1; $i <= 20; $i++)
+{
+    for($j = 0; $j < $i; $j++)
+    {
+        echo '-';
+    }
 
-$nomeCompleto = $nome;
-$nomeCompleto .= $sobrenome ?? '';
+    echo '<br/>';
+}
 
-echo $nomeCompleto;
+echo '<br/>';
+
+for($i = 0; $i < 10; $i++)
+{
+    for($j = 0; $j < 10; $j++)
+    {
+        echo '-';
+    }
+
+    echo '<br/>';
+}
