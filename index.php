e85a04b67a48c8576f7245a86a8082d99f91efca
Athos Francisco Pereira do Carmo <athos.carmo@mcmmontagens.com.br>
2021-04-19 21:43:34 -0300

    Operador Ternário Null CAO. Só funciona a partir 7.4

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
 <?php
 
-$lista = [
-    'nome' => 'Athos',
-    'idade' => 100,
-    'atributos' => [
-        'forca' => 60,
-        'agilidade' => 80,
-        'destreza' => 50
-    ],
-    'vida' => 1000,
-    'mana' => 928
-];
-?>
-<pre>
-    <?php print_r($lista); ?>
-</pre>
+$nome = 'Athos';
+
+$nomeCompleto = $nome;
+$nomeCompleto .= $sobrenome ?? '';
+
+echo $nomeCompleto;
