2c60e2cf686e307ce08c6b65143533c930d1a247
Athos Francisco Pereira do Carmo <athos.carmo@mcmmontagens.com.br>
2021-04-19 21:17:51 -0300

    desafio

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
 <?php
 
-$ingredientes = [
-    'farinha',
-    'ovo',
-    'açúcar'
-];
-
-$bolo2 = [
-    'chocolate',
-    ...$ingredientes,
-    'leite'
+$lista = [
+    'nome' => 'Athos',
+    'idade' => 100,
+    'atributos' => [
+        'forca' => 60,
+        'agilidade' => 80,
+        'destreza' => 50
+    ],
+    'vida' => 1000,
+    'mana' => 928
 ];
 ?>
-
 <pre>
-
-<?php print_r($bolo2); ?>
-
+    <?php print_r($lista); ?>
 </pre>
